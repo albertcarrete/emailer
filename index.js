900454c5e6577ea92a0cfea192a4494854d885c5
Albert Carrete <acarrete@Alberts-MacBook-Pro.local>
2017-10-05 02:06:39 -0700

    handler for passport js and google oauth

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-// app - express app to register this route handler with
-// get - request handler, watch for incoming requests with this method
-// '/' watch for requests trying to access '/'
-// req -- object representing the incoming request
-// res -- object representing the outgoing response
-// res.send({hi:'there}) -- immediately send some JSON back to who ever made this request
-app.get('/', (req,res) => {
-    res.send({hi: 'there'});
-})
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 // check if we are in a development environment
 // check for Heroku's port
 const PORT = process.env.PORT || 5000;
-// Express here is really just telling 
+// Express here is really just telling
 // Node.js to watch for traffic on this port
 app.listen(PORT);
