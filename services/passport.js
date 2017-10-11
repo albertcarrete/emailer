091d5fec3d81305e441a1fbf288bb7db6cba5a9d
Albert Carrete <acarrete@Alberts-MacBook-Pro.local>
2017-10-10 22:34:54 -0700

    added react js files and async await

--- services/passport.js
+++ services/passport.js
@@ -24,19 +24,17 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a record with the given profile id
-          // first argument is an error, second is the user
-          done(null, existingUser);
-        } else {
-          // we don't have a record with this id, make a new record
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // we already have a record with the given profile id
+        // first argument is an error, second is the user
+        done(null, existingUser);
+      } else {
+        // we don't have a record with this id, make a new record
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
